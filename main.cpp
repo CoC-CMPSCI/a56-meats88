0df6ac487b3658822ebabe2d7601ce3192c5c79d
meats88 <pcx883mi@gmail.com>
2026-06-02 19:28:47 -0700

    doned

--- main.cpp
+++ main.cpp
@@ -8,4 +8,11 @@ int main()
 	cin >> N;
 
 	// TODO
+	for (int i = 0; i < N; i++) {
+        for (int j = N - 1 - i; j < N; j++) {
+            cout << i << " , " << j << endl;
+        }
+    }
+
+    return 0;
 }
